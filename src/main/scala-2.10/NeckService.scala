03e626d3e1a609ca34ec47a67c0e97a18d704c8f
Mark Kelly <mkelly@zalando.ie>
2015-12-16 17:53:04 +0000

    * NeckService

---
+++ src/main/scala-2.10/NeckService.scala
@@ -0,0 +1,10 @@
+import org.bytedeco.javacpp.opencv_core.{Point, Rect, Mat}
+
+object NeckService {
+
+  def findSize(image : Mat, heightInches : Double) : Double = {
+    val faceRect : Rect = FaceIdentifier.find(image)
+    val neckPoints : (Point, Point) = NeckIdentifier.find(image, faceRect)
+    NeckSizeEstimation.findNeckSizeInInches(heightInches, faceRect, neckPoints)
+  }
+}
